b3d2103fa00b24a83c6707f93533a101ce1e349c
Fayepasvouri <45490922+Fayepasvouri@users.noreply.github.com>
2020-09-06 11:29:06 +0100

    Create train_test.py

---
+++ train_test.py
@@ -0,0 +1,192 @@
+"""
+Faye
+"""
+
+
+import keras
+from keras import regularizers
+from keras.preprocessing import sequence
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential, Model, model_from_json
+from keras.layers import Dense, Embedding, LSTM
+from keras.layers import Input, Flatten, Dropout, Activation, BatchNormalization
+from keras.layers import Conv1D, MaxPooling1D, AveragePooling1D
+from keras.utils import np_utils, to_categorical
+from keras.callbacks import ModelCheckpoint
+
+# sklearn
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+# Other  
+import librosa
+import librosa.display
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from matplotlib.pyplot import specgram
+import pandas as pd
+import seaborn as sns
+import glob 
+import os
+import pickle
+import IPython.display as ipd
+
+ref = pd.read_csv("C:/Users/Faye/Desktop/Master/Data_path.csv")
+ref.head()
+
+df = pd.DataFrame(columns=['feature'])
+
+# loop feature extraction over the entire dataset
+counter=0
+for index,path in enumerate(ref.path):
+    X, sample_rate = librosa.load(path
+                                  , res_type='kaiser_fast'
+                                  ,duration=2.5
+                                  ,sr=44100
+                                  ,offset=0.5
+                                 )
+    sample_rate = np.array(sample_rate)
+    
+    # mean as the feature. Could do min and max etc as well. 
+    mfccs = np.mean(librosa.feature.mfcc(y=X, 
+                                        sr=sample_rate, 
+                                        n_mfcc=13),
+                    axis=0)
+    df.loc[counter] = [mfccs]
+    counter=counter+1   
+
+# Check a few records to make sure its processed successfully
+print(len(df))
+df.head()
+
+df = pd.concat([ref,pd.DataFrame(df['feature'].values.tolist())],axis=1)
+df[:5]
+
+df=df.fillna(0)
+print(df.shape)
+df[:5]
+
+X_train, X_test, y_train, y_test = train_test_split(df.drop(['path','labels','source'],axis=1)
+                                                    , df.labels
+                                                    , test_size=0.25
+                                                    , shuffle=True
+                                                    , random_state=42
+                                                   )
+
+# Lets see how the data present itself before normalisation 
+X_train[150:160]
+
+mean = np.mean(X_train, axis=0)
+std = np.std(X_train, axis=0)
+
+X_train = (X_train - mean)/std
+X_test = (X_test - mean)/std
+
+# Check the dataset now 
+X_train[150:160]
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
+# one hot encode the target 
+lb = LabelEncoder()
+y_train = np_utils.to_categorical(lb.fit_transform(y_train))
+y_test = np_utils.to_categorical(lb.fit_transform(y_test))
+
+print(X_train.shape)
+print(lb.classes_)
+#print(y_train[0:10])
+#print(y_test[0:10])
+
+# Pickel the lb object for future use 
+filename = 'labels'
+outfile = open(filename,'wb')
+pickle.dump(lb,outfile)
+outfile.close()
+
+model = Sequential()
+model.add(Conv1D(256, 8, padding='same',input_shape=(X_train.shape[1],1)))  # X_train.shape[1] = No. of Columns
+model.add(Activation('relu'))
+model.add(Conv1D(256, 8, padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+model.add(MaxPooling1D(pool_size=(8)))
+model.add(Conv1D(128, 8, padding='same'))
+model.add(Activation('relu'))
+model.add(Conv1D(128, 8, padding='same'))
+model.add(Activation('relu'))
+model.add(Conv1D(128, 8, padding='same'))
+model.add(Activation('relu'))
+model.add(Conv1D(128, 8, padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+model.add(MaxPooling1D(pool_size=(8)))
+model.add(Conv1D(64, 8, padding='same'))
+model.add(Activation('relu'))
+model.add(Conv1D(64, 8, padding='same'))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(14)) # Target class number
+model.add(Activation('softmax'))
+# opt = keras.optimizers.SGD(lr=0.0001, momentum=0.0, decay=0.0, nesterov=False)
+# opt = keras.optimizers.Adam(lr=0.0001)
+opt = keras.optimizers.RMSprop(lr=0.00001, decay=1e-6)
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer=opt,metrics=['accuracy'])
+model_history=model.fit(X_train, y_train, batch_size=16, epochs=100, validation_data=(X_test, y_test))
+
+plt.plot(model_history.history['loss'])
+plt.plot(model_history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+model_name = 'Emotion_Model.h5'
+save_dir = os.path.join(os.getcwd(), 'saved_models')
+
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+model_path = os.path.join(save_dir, model_name)
+model.save(model_path)
+print('Save model and weights at %s ' % model_path)
+
+# Save the model to disk
+model_json = model.to_json()
+with open("model_json.json", "w") as json_file:
+    json_file.write(model_json)
+
+json_file = open('model_json.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+loaded_model.load_weights("saved_models/Emotion_Model.h5")
+print("Loaded model from disk")
+ 
+# Keras optimiser
+opt = keras.optimizers.rmsprop(lr=0.00001, decay=1e-6)
+loaded_model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+score = loaded_model.evaluate(X_test, y_test, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+
+
+
+preds = loaded_model.predict(X_test, 
+                         batch_size=16, 
+                         verbose=1)
+
+preds=preds.argmax(axis=1)
+preds
+
